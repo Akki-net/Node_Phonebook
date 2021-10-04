f1ce812f47cc8558d7490cafb1ff547bd1c9252e
Akash Sharma <akashsharma.zak@gmail.com>
2021-10-04 13:49:49 +0530

    source code for Project

---
+++ index.js
@@ -0,0 +1,67 @@
+const { application } = require('express');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+    { 
+        id: 1,
+        name: "Arto Hellas", 
+        number: "040-123456"
+      },
+      { 
+        id: 2,
+        name: "Ada Lovelace", 
+        number: "39-44-5323523"
+      },
+      { 
+        id: 3,
+        name: "Dan Abramov", 
+        number: "12-43-234345"
+      },
+      { 
+        id: 4,
+        name: "Mary Poppendieck", 
+        number: "39-23-6423122"
+      }
+];
+
+app.get('/',(req, res) => {
+    res.send('<h1>Hello World</h1>')
+});
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+});
+
+app.get('/info', (req, res) => {
+    const len = persons.length;
+    const d = new Date();
+    const str = `<div> <p>Phonebook has info for ${len} people</p>
+    <p> ${d.toString()} </p> </div>`;
+    res.send(str)
+});
+
+app.get('/api/persons/:id', (req,res) => {
+    const id = Number(req.params.id);
+    const person = persons.find(p => p.id === id);
+
+    if(person){
+        res.json(person);
+    }
+    else{
+        res.status(404).end()
+    }
+});
+
+app.delete('/api/persons/:id', (req,res) => {
+    const id = Number(req.params.id);
+    persons = persons.filter(p => p.id !== id);
+    res.status(202).end()
+});
+
+const Port = 3001;
+app.listen(Port,() => {
+    console.log(`Server is running on Port ${Port}`)
+});
